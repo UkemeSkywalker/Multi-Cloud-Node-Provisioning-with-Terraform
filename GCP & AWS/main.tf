a82c6ef92ba5011c35614b8f5c572225f3afbad5
Ukeme David Eseme <skywalkershadow9@gmail.com>
2024-02-23 16:44:05 +0000

    updates

--- GCP & AWS/main.tf
+++ GCP & AWS/main.tf
@@ -1,5 +1,14 @@
+provider "google" {
+  credentials = file("path/to/google-credentials.json")
+}
+
 provider "aws" {
   region     = "us-east-1" # Default region for AWS provider
+}
+
+module "gcp_nodes" {
+  source     = "./modules/gcp_nodes"
+
 }
 
 module "aws_nodes" {
@@ -7,5 +16,5 @@ module "aws_nodes" {
 }
 
 output "provisioned_nodes_report" {
-  value = concat(module.aws_nodes.provisioned_aws_nodes)
+  value = concat(module.aws_nodes.provisioned_aws_nodes, module.gcp_nodes.provisioned_gcp_nodes)
 }
